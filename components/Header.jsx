a9cf6ca8e012a87b5e7dfcabe34d6f059511b95d
txpham98 <txpham98@gmail.com>
2025-06-03 15:04:10 -0700

    Update Header.jsx

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,7 +1,6 @@
 export default function Header() {
     return (
         <header>
-            <img src="../images/globe.png" alt="globe icon" />
             <h1>my travel journal.</h1>
         </header>
     )
